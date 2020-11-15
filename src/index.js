3e9a5035c4c1ac440784d4d4d42af568c19efef6
Fiveolioo <96richardohc@gmail.com>
2020-11-14 21:29:47 -1000

    commit tree 1e6c7f336ffdee1f445900169367073e90480bdf

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+function fetchDogs() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        console.log('json', json)
+      renderDogImages(json);
+    });
+  }
+  
+  function renderDogImages(images) {
+    const dogImageContainer = document.getElementById('dog-image-container')
+    images.forEach(image => {
+      const img = document.createElement('img')
+      img.src = image
+      dogImageContainer.appendChild(img)
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchDogs()
+  })
